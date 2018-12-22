334e2747ebfa8c731dfe05813f4de2d1df30f2bc
Trần Duy Báu <trdbau@gmail.com>
2018-12-22 21:36:15 +0700

    add actonTypes

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: 0,
+    "import/extensions": 0,
+    "import/extensions": 0,
+    "no-param-reassign": 0
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
